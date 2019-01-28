613b048081f0d25b6cb82226e5227f2b34afac50
Jimmy Pouliot <jpouliot@lvlstudio.com>
2019-01-28 17:45:36 -0500

    Add Owner and Pets templates and controllers. Standardize mappings.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/PetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/PetsController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping({"/vets"})
-public class VetsController {
+@RequestMapping({"/pets"})
+public class PetsController {
 
     @RequestMapping({""})
     public String index() {
-        return "vets";
+        return "pets";
     }
 }
